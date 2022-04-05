76eabc7ff514634c2d673771031b85f076c6e788
Thomas Kanzig <thomas.kanzig@gmail.com>
2022-04-05 22:40:57 +0200

    Added some static HTML

--- assets/js/pages/ProducDetailPage.tsx
+++ assets/js/pages/ProducDetailPage.tsx
@@ -6,8 +6,23 @@ const ProductDetailPage = () => {
     return (
         <Layout>
             <div className="container">
-                <h1>Product detail</h1>
-                <Link to="/products">Back to overview</Link>
+                <Link to="/products">
+                    Go back to overview
+                </Link>
+
+                <h1 className="my-5">Name of Product</h1>
+                <div className="row">
+                    <div className="col-sm-12 col-md-4 mb-4">
+                        <img src="https://picsum.photos/800/600" className="rounded d-block img-fluid" alt="Image"/>
+                    </div>
+                    <div className="col-sm-12 col-md-8">
+                        <p>
+                            There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.
+                        </p>
+                        <h3>89,99</h3>
+                        <div className="product-detail__article-number">Article number: 0001</div>
+                    </div>
+                </div>
             </div>
         </Layout>
     );
--- assets/js/pages/ProductListPage.tsx
+++ assets/js/pages/ProductListPage.tsx
@@ -6,8 +6,47 @@ const ProductListPage = () => {
     return (
         <Layout>
             <div className="container">
-                <h1>Product list</h1>
-                <Link to="/products/asdsadas">See an example of detail product</Link>
+                <h1 className="mb-5">Product list</h1>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Image</th>
+                            <th>Article number</th>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>
+                                <img src="https://picsum.photos/60/60" className="rounded d-block product-list__image" alt="Image"/>
+                            </th>
+                            <td>0001</td>
+                            <td>Name of Product</td>
+                            <td>89.00 EUR</td>
+                            <th>
+                                <Link to="/products/0001" className="btn btn-primary">
+                                    <i className="bi bi-eye-fill"/> Detail
+                                </Link>
+                            </th>
+                        </tr>
+
+                        <tr>
+                            <th>
+                                <img src="https://picsum.photos/60/60" className="rounded d-block" alt="Image"/>
+                            </th>
+                            <td>0002</td>
+                            <td>Name of Product</td>
+                            <td>67.00 EUR</td>
+                            <th>
+                                <Link to="/products/0001" className="btn btn-primary">
+                                    <i className="bi bi-eye-fill"/> Detail
+                                </Link>
+                            </th>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </Layout>
     );
